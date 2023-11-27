13dfa08665094fdf26210b768318a2ff318a455c
usernameRafael-Vitor-Oliveira <rafaelvitordeoliveira1@gmail.com>
2023-11-26 23:50:39 -0300

    first commit and final stretch of the project

---
+++ README.md
@@ -0,0 +1,27 @@
+<h1 align="center">Página de ajuda</h1>
+
+<p align="center">
+<br/>
+</p>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+</p>
+<br>
+<p align="center">
+  <img alt="Home Page" src=".github/homePage.JPG" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML, CSS e JavaScript
+- Git e Github
+
+## 💻 Projeto
+
+O intuito do projeto é uma página web para ajudar pessoas, com dificuldade em realizar atividades comuns na internet, como limpar o cache do navegador, baixar um aplicativo, entre outros. A página foi pensada como um TCC do meu curso de análise e desenvolvimento de sistema, da faculdade. 
+
+- [Visit the project online](https://rafael-vitor-oliveira.github.io/HelpPage/)
---
+++ script.js
@@ -0,0 +1,74 @@
+const openModalPrint = document.querySelector('#openModalPrint')
+const modalPrint = document.querySelector('.modalPrint')
+const buttonClosePrint = document.querySelector('.buttonClosePrint')
+
+const openModalCache = document.querySelector('#openModalCache')
+const modalCache = document.querySelector('.modalCache')
+const buttonCloseCache = document.querySelector('.buttonCloseCache')
+
+const openModalAccessibility = document.querySelector('#openModalAccessibility');
+const modalAccessibility = document.querySelector('.modalAccessibility');
+const buttonCloseAccessibility = document.querySelector('.buttonCloseAccessibility');
+
+const enlargeButton = document.querySelector('.enlargeButton')
+const searchBox = document.querySelector('.searchBox')
+const boxHelp = document.querySelectorAll('.boxTextHelp')
+const textHelp = document.querySelector('.textHelp')
+
+function search(){
+    if(searchBox.value != ''){
+        for (let boxes of boxHelp){
+            let title = boxes.querySelector('.textHelp')
+
+            title = title.textContent.toLowerCase()
+            let filterText = searchBox.value.toLowerCase()
+            if(!title.includes(filterText)){
+               boxes.style.display = 'none'
+            }
+            else{
+                boxes.style.display = 'block'
+            }
+        }
+    }
+    else{
+        for(let boxes of boxHelp){
+            boxes.style.display = 'block'
+        }
+    }
+}
+
+openModalPrint.onclick = function (){
+    modalPrint.showModal()
+}
+buttonClosePrint.onclick = function (){
+    modalPrint.close()
+}
+
+openModalCache.onclick = function (){
+    modalCache.showModal()
+}
+buttonCloseCache.onclick = function(){
+    modalCache.close()
+}
+
+openModalAccessibility.onclick = function (){
+    modalAccessibility.showModal()
+}
+buttonCloseAccessibility.onclick = function (){
+    modalAccessibility.close()
+}
+
+function ampliar(){
+    let buttonZoom = document.getElementById('info');
+    buttonZoom.classList.toggle('infoZoom');
+
+    if(buttonZoom.classList.contains('infoZoom')){
+        let buttonZoomOut = document.querySelector('.enlargeButton');
+        buttonZoomOut.setAttribute('src', './assets/diminuir.svg');
+    }
+    else{
+        let buttonZoomIn = document.querySelector('.enlargeButton');
+        buttonZoomIn.setAttribute('src', './assets/ampliar2.svg');
+    }
+}
+searchBox.addEventListener('input', search);
