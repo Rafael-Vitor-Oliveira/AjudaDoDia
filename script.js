540b98b6badf7617455ed7367de37388896fb145
usernameRafael-Vitor-Oliveira <rafaelvitordeoliveira1@gmail.com>
2023-12-01 16:48:04 -0300

    Adding suggestion button

--- script.js
+++ script.js
@@ -1,63 +1,73 @@
-const openModalPrint = document.querySelector('#openModalPrint')
-const modalPrint = document.querySelector('.modalPrint')
-const buttonClosePrint = document.querySelector('.buttonClosePrint')
+const openModalPrint = document.querySelector('#openModalPrint');
+const modalPrint = document.querySelector('.modalPrint');
+const buttonClosePrint = document.querySelector('.buttonClosePrint');
 
-const openModalCache = document.querySelector('#openModalCache')
-const modalCache = document.querySelector('.modalCache')
-const buttonCloseCache = document.querySelector('.buttonCloseCache')
+const openModalCache = document.querySelector('#openModalCache');
+const modalCache = document.querySelector('.modalCache');
+const buttonCloseCache = document.querySelector('.buttonCloseCache');
 
 const openModalAccessibility = document.querySelector('#openModalAccessibility');
 const modalAccessibility = document.querySelector('.modalAccessibility');
 const buttonCloseAccessibility = document.querySelector('.buttonCloseAccessibility');
 
-const enlargeButton = document.querySelector('.enlargeButton')
-const searchBox = document.querySelector('.searchBox')
-const boxHelp = document.querySelectorAll('.boxTextHelp')
-const textHelp = document.querySelector('.textHelp')
+const suggestionBox = document.querySelector('.suggestionBox');
+const modalSuggestion = document.querySelector('.modalSuggestion');
+const buttonCloseSuggestion = document.querySelector('.buttonCloseSuggestion');
+
+const enlargeButton = document.querySelector('.enlargeButton');
+const searchBox = document.querySelector('.searchBox');
+const boxHelp = document.querySelectorAll('.boxTextHelp');
+const textHelp = document.querySelector('.textHelp');
 
 function search(){
     if(searchBox.value != ''){
         for (let boxes of boxHelp){
-            let title = boxes.querySelector('.textHelp')
+            let title = boxes.querySelector('.textHelp');
 
-            title = title.textContent.toLowerCase()
-            let filterText = searchBox.value.toLowerCase()
+            title = title.textContent.toLowerCase();
+            let filterText = searchBox.value.toLowerCase();
             if(!title.includes(filterText)){
-               boxes.style.display = 'none'
+               boxes.style.display = 'none';
             }
             else{
-                boxes.style.display = 'block'
+                boxes.style.display = 'block';
             }
         }
     }
     else{
         for(let boxes of boxHelp){
-            boxes.style.display = 'block'
+            boxes.style.display = 'block';
         }
     }
 }
 
 openModalPrint.onclick = function (){
-    modalPrint.showModal()
+    modalPrint.showModal();
 }
 buttonClosePrint.onclick = function (){
-    modalPrint.close()
+    modalPrint.close();
 }
 
 openModalCache.onclick = function (){
-    modalCache.showModal()
+    modalCache.showModal();
 }
 buttonCloseCache.onclick = function(){
-    modalCache.close()
+    modalCache.close();
 }
 
 openModalAccessibility.onclick = function (){
-    modalAccessibility.showModal()
+    modalAccessibility.showModal();
 }
 buttonCloseAccessibility.onclick = function (){
-    modalAccessibility.close()
+    modalAccessibility.close();
 }
 
+suggestionBox.onclick = function (){
+    modalSuggestion.showModal();
+}
+buttonCloseSuggestion.onclick = function(){
+    modalSuggestion.close()
+}
 function ampliar(){
     let buttonZoom = document.getElementById('info');
     buttonZoom.classList.toggle('infoZoom');
