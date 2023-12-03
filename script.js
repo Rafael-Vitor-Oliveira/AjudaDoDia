548e6a73620b0bc3c1a6e66ba8515d56f8a56b84
usernameRafael-Vitor-Oliveira <rafaelvitordeoliveira1@gmail.com>
2023-12-03 19:33:35 -0300

    Inserting suggestion sending function

--- script.js
+++ script.js
@@ -13,11 +13,14 @@ const buttonCloseAccessibility = document.querySelector('.buttonCloseAccessibili
 const suggestionBox = document.querySelector('.suggestionBox');
 const modalSuggestion = document.querySelector('.modalSuggestion');
 const buttonCloseSuggestion = document.querySelector('.buttonCloseSuggestion');
+const buttonSendSuggestion = document.querySelector('.buttonSendSuggestion');
 
 const enlargeButton = document.querySelector('.enlargeButton');
 const searchBox = document.querySelector('.searchBox');
 const boxHelp = document.querySelectorAll('.boxTextHelp');
 const textHelp = document.querySelector('.textHelp');
+
+const suggestionTxt = document.querySelector('.suggestionTxt');
 
 function search(){
     if(searchBox.value != ''){
@@ -65,8 +68,24 @@ buttonCloseAccessibility.onclick = function (){
 suggestionBox.onclick = function (){
     modalSuggestion.showModal();
 }
-buttonCloseSuggestion.onclick = function(){
-    modalSuggestion.close()
+buttonCloseSuggestion.onclick = function (){
+    modalSuggestion.close();
+    pictureImage.innerHTML = '';
+    pictureImage.innerHTML = pictureImageTxt;
+    suggestionTxt.value = '';
+}
+buttonSendSuggestion.onclick = function (){
+
+    if(suggestionTxt.value == ''){
+
+        alert('Você deve descrever sua sugestão para poder enviar');
+    }else{
+        pictureImage.innerHTML = '';
+        pictureImage.innerHTML = pictureImageTxt;
+        suggestionTxt.value = '';
+    
+        alert('Sua sugestão foi enviada');
+    }
 }
 function ampliar(){
     let buttonZoom = document.getElementById('info');
@@ -82,3 +101,36 @@ function ampliar(){
     }
 }
 searchBox.addEventListener('input', search);
+
+const inputFile = document.querySelector('#pictureInputID');
+const pictureImage = document.querySelector('.pictureImage');
+const pictureImageTxt = 'Selecione uma imagem de exemplo';
+pictureImage.innerHTML = pictureImageTxt;
+
+inputFile.addEventListener('change', function(e){
+    const inputTarget = e.target;
+    const file = inputTarget.files[0];
+
+    if(file){
+        const reader = new FileReader();
+
+        reader.addEventListener('load', function(e){
+            const readerTarget = e.target;
+
+            const addImg = document.createElement('img');
+            addImg.src = readerTarget.result;
+            addImg.classList.add('pictureImg');
+            addImg.setAttribute('id', 'imgInsert');
+            pictureImage.innerHTML = '';
+            pictureImage.appendChild(addImg);
+        });
+        reader.readAsDataURL(file);
+    };
+});
+const buttonDelete = document.querySelector('.deletarImagem');
+const imageInsert = document.querySelector('#imgInsert');
+
+buttonDelete.onclick = function(){
+    pictureImage.innerHTML = '';
+    pictureImage.innerHTML = pictureImageTxt;
+}
